e504fcea4199c372eb8d739c27dc1c1c65518d77
Alexander Kobelev <slenys@rambler.ru>
2023-04-21 16:52:45 +0300

    Change Chapter2 source code

---
+++ KafkaInAction_Chapter2/CSharp/Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using Confluent.Kafka;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Gets all messages that were sended before to kinaction_helloworld topc
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092,localhost:9093,localhost:9094",
+                GroupId = "kinaction_helloconsumer",
+                AutoOffsetReset = AutoOffsetReset.Latest,
+            };
+
+            using (var c = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                c.Subscribe("kinaction_helloworld");
+
+                try
+                {
+                    while(true)
+                    {
+                        var cr = c.Consume(1000);
+
+                        if (cr != null)
+                            Console.WriteLine($"Consumed message '{cr.Value}' ofsset: '{cr.TopicPartitionOffset}' Partition: {cr.Partition}");
+                    }                   
+                }
+                catch (ConsumeException e)
+                {
+                    Console.WriteLine($"Error occured: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
---
+++ KafkaInAction_Chapter2/CSharp/Producer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Confluent.Kafka;
+
+namespace Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092,localhost:9093,localhost:9094" };
+
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                int number = 0;
+                while (true)
+                {
+                    try
+                    {
+                        Thread.Sleep(1000);
+                        var responce = p.ProduceAsync("kinaction_helloworld", new Message<Null, string> { Value = $"hello world again! {number++}" });
+                        responce.Wait();
+                        Console.WriteLine($"{responce.Result.Value} {responce.Result.Status} {responce.Result.Partition}");
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+
+            }
+        }
+    }
+}
